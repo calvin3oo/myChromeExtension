c1d55a200f65bcbdcc20b3d38734d388883f7ed2
calvin3oo <calvinzboss@gmail.com>
2023-10-19 17:01:53 -0600

    working on extension

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -1,3 +1,4 @@
+import { CANVAS_TOKEN } from './secrets.js';
 // This code/function should allow the 'chrome popup' to be able to access things in the DOM outside of the popup
 const reasonTypes = chrome.offscreen.Reason;
 async function createOffscreen() {
@@ -9,6 +10,67 @@ async function createOffscreen() {
         justification: "testing",
     });
 }
-console.log("Hello World");
-console.log("from inside chrome ext");
-console.log(document.querySelectorAll('div'));
+function main() {
+    // env var
+    console.log(CANVAS_TOKEN);
+    // localstorage
+    console.log("writing to localstorage");
+    writeJSONToLocalStorage("test", "hello world");
+    console.log("reading from localstorage");
+    console.log(readJSONFromLocalStorage("test"));
+    createOffscreen();
+    triggerEvent();
+}
+function triggerEvent() {
+    // Send a "Hello, World!" message to the content script
+    // chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    //   chrome.scripting.executeScript({
+    //     target: { tabId: tabs[0].id },
+    //     func: () => {
+    //       chrome.runtime.sendMessage({ greeting: 'Hello, World!' });
+    //     },
+    //   });
+    // });
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function (tabs) {
+        chrome.scripting.executeScript({
+            target: {
+                tabId: tabs[0].id
+            },
+            func: () => {
+                console.log("sending message");
+                // trigger custom event on window
+                window.dispatchEvent(new CustomEvent('custom'));
+            },
+        });
+    });
+}
+// Function to write JSON data to localStorage
+function writeJSONToLocalStorage(key, data) {
+    try {
+        const jsonData = JSON.stringify(data);
+        localStorage.setItem(key, jsonData);
+        return true;
+    }
+    catch (error) {
+        console.error("Error writing JSON to localStorage:", error);
+        return false;
+    }
+}
+// Function to read JSON data from localStorage
+function readJSONFromLocalStorage(key) {
+    try {
+        const jsonData = localStorage.getItem(key);
+        if (jsonData === null) {
+            return null;
+        }
+        return JSON.parse(jsonData);
+    }
+    catch (error) {
+        console.error("Error reading JSON from localStorage:", error);
+        return null;
+    }
+}
+main();
--- src/onPagesLoading.ts
+++ src/onPagesLoading.ts
@@ -1,3 +1,12 @@
 // This js file will run when the page is loaded
-console.log('this page is loaded')
+window.addEventListener('custom', function () {
+    console.log('custom event triggered');
+});
 
+function printHello(){
+    setTimeout(() => {
+        console.log('HELLO THERE 3')
+        printHello()
+    }, 1000);
+}
+printHello()
